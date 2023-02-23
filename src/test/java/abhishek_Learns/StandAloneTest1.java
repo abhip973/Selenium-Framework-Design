efb8aabd3a698739ebaba4bb73dc1092a68eac06
abhipunj01 <abhishek.punj@dentsu.com>
2023-02-23 10:59:47 +0530

    Framework Design Day 7

--- src/test/java/abhishek_Learns/StandAloneTest1.java
+++ src/test/java/abhishek_Learns/StandAloneTest1.java
@@ -4,24 +4,27 @@ import PageObjects.*;
 
 import abhishek_Learns.TestComponents.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 
 public class StandAloneTest1 extends BaseTest {
-    String productName = "ZARA COAT 3";
+    //    String productName = "ZARA COAT 3";
+    String productName;
 
-    @Test
-    public void submitOrder() throws IOException, InterruptedException {
-//        String productName = "ZARA COAT 3";
+    @Test(dataProvider = "getData")
+    public void submitOrder(String email, String pwd, String pName) throws IOException, InterruptedException {
+//        String productName;
         String confirmationMsg = "THANKYOU FOR THE ORDER.";
         String country = "India";
 
 
-        ProductCatalogue productCatalogue = landingPage.loginToApplication("punj.abhishek1@gmail.com", "Abhi@123");
+        ProductCatalogue productCatalogue = landingPage.loginToApplication(email, pwd);
 
 
+        productName = pName;
         productCatalogue.getProductsList();
         productCatalogue.getProductByName(productName);
         productCatalogue.addProductToCart(productName);
@@ -50,5 +53,19 @@ public class StandAloneTest1 extends BaseTest {
         Boolean orderFoundActual = orderPage.verifyOrderDisplay(productName);
         Assert.assertTrue(orderFoundActual);
 
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Object[][] obj = new Object[2][3];
+
+        obj[0][0] = "punj.abhishek1@gmail.com";
+        obj[0][1] = "Abhi@123";
+        obj[0][2] = "ADIDAS ORIGINAL";
+
+        obj[1][0] = "ap5@hotmail.com";
+        obj[1][1] = "Abhi1234";
+        obj[1][2] = "ZARA COAT 3";
+        return obj;
     }
 }
