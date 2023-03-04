cf3d018d278df9db7f2328e2aabf573861606862
abhip973 <punj.abhishek1@gmail.com>
2023-03-04 12:53:26 +0530

    Retry and Thread Safety

--- src/test/java/abhishek_Learns/ErrorValidations.java
+++ src/test/java/abhishek_Learns/ErrorValidations.java
@@ -1,20 +1,17 @@
 package abhishek_Learns;
 
 import abhishek_Learns.TestComponents.BaseTest;
+import abhishek_Learns.TestComponents.Retry;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ErrorValidations extends BaseTest {
 
-    @Test(groups = {"ErrorHandling"})
+    @Test(groups = {"ErrorHandling"}, retryAnalyzer = Retry.class)
     public void loginErrorValidations() {
         landingPage.loginToApplication("punj.abhi@gmail.com", "Abhi@1234");
         String errorMsgText = landingPage.getErrorMsg();
         Assert.assertEquals(errorMsgText, "Incorrect email  password.");
     }
 
-
-    public void productErrorValidations() {
-
-    }
 }
